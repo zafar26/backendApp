55a0a6f65cfa95387fda9897b22d71e2e78502ef
zafar26 <qutubuddinzafar2616118@gmail.com>
2020-09-29 12:32:45 +0530

    Added Booking Api

--- app.js
+++ app.js
@@ -24,17 +24,19 @@ app.use('/position', require('./routes/position.router'));
 app.use('/employee', require('./routes/employee.router'));
 app.use('/bus', require('./routes/bus.router'));
 app.use('/hotel', require('./routes/hotel.router'));
+// app.use('/', require('./routes/hotel.router'));
+
 
 
 sequelize.connection()
     .authenticate()
     .then(() => {
-            return sequelize.connection().sync({force: false });
+        return sequelize.connection().sync({ force: false });
     })
     .then(() => {
         app.listen(config.port, (err) => {
-            if(err) throw err;
-            else console.log("\x1b[32m%s\x1b[0m", 'listening on: http://localhost:'+config.port);
+            if (err) throw err;
+            else console.log("\x1b[32m%s\x1b[0m", 'listening on: http://localhost:' + config.port);
         });
     })
     .catch(err => console.error(err));
